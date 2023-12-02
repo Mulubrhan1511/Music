daa91f8f895df00c0b90f0d4c247a30a66240071
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-12-02 16:06:50 +0300

    I did the nav bar and the home page

--- front/src/components/screen/Home.js
+++ front/src/components/screen/Home.js
@@ -22,7 +22,30 @@ function Home() {
       <p>It is a great day</p>
     </div>
   </div>
-
+  <div className="card">
+    <a href="#!">
+      <img src="https://res.cloudinary.com/dhw1mueq4/image/upload/v1701521272/download_2_zlimc5.jpg" alt="Hollywood Sign on The Hill" />
+    </a>
+    <div className="content">
+      <p>It is a great day</p>
+    </div>
+  </div>
+  <div className="card">
+    <a href="#!">
+      <img src="https://res.cloudinary.com/dhw1mueq4/image/upload/v1701521272/download_2_zlimc5.jpg" alt="Hollywood Sign on The Hill" />
+    </a>
+    <div className="content">
+      <p>It is a great day</p>
+    </div>
+  </div>
+  <div className="card">
+    <a href="#!">
+      <img src="https://res.cloudinary.com/dhw1mueq4/image/upload/v1701521272/download_2_zlimc5.jpg" alt="Hollywood Sign on The Hill" />
+    </a>
+    <div className="content">
+      <p>It is a great day</p>
+    </div>
+  </div>
   <div className="card">
     <a href="#!">
       <img src="https://res.cloudinary.com/dhw1mueq4/image/upload/v1701521272/download_2_zlimc5.jpg" alt="Hollywood Sign on The Hill" />
