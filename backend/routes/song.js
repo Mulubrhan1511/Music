20ae3fdb4de2928e44a9444de45a00c876eedb56
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-12-04 17:38:59 +0300

    i did every thing

--- backend/routes/song.js
+++ backend/routes/song.js
@@ -24,6 +24,18 @@ router.post('/songs',(req, res) => {
  })
 });
 
+router.get('/song/:songid', async (req, res) => {
+    try {
+      const song = await Song.findOne({ _id: req.params.songid });
+      res.json({ song });
+    } catch (err) {
+      return res.status(404).json({ error: "Song not found" });
+    }
+  });
+
+
+
+
 // List all songs
 router.get('/songs', async (req, res) => {
   try {
@@ -35,7 +47,7 @@ router.get('/songs', async (req, res) => {
 });
 
 // Update a song
-router.put('/songs/', async (req, res) => {
+router.put('/songs', async (req, res) => {
     try {
       const { _id,title, artist, album, genre } = req.body;
       const song = await Song.findByIdAndUpdate(_id, { title, artist, album, genre }, { new: true });
@@ -50,13 +62,52 @@ router.put('/songs/', async (req, res) => {
   router.delete('/songs/:id', async (req, res) => {
     try {
       const { id } = req.params;
-      await Song.findByIdAndRemove(id);
-      res.json("Song removed successfully");
+      const result = await Song.findOneAndDelete({ _id: id });
+      if (result) {
+        console.log("Song removed");
+        res.json("Song removed successfully");
+      } else {
+        throw new Error("Failed to remove the song");
+      }
     } catch (error) {
+      console.log(error);
       res.status(400).json({ error: 'Failed to remove the song' });
     }
   });
   
   
+  
+// Get overall statistics
+router.get('/stats', async (req, res) => {
+  try {
+    const totalSongs = await Song.countDocuments();
+    const songsByGenre = await Song.aggregate([
+      { $group: { _id: '$genre', count: { $sum: 1 } } }
+    ]);
+    const songsByArtist = await Song.aggregate([
+      { $group: { _id: '$artist', count: { $sum: 1 }, albums: { $addToSet: '$album' } } }
+    ]);
+    const songsByAlbum = await Song.aggregate([
+      { $group: { _id: '$album', count: { $sum: 1 }, artist: { $first: '$artist' } } }
+    ]);
+    const totalArtists = songsByArtist.length;
+    const totalAlbums = songsByAlbum.length;
+    const totalGenres = songsByGenre.length;
+    res.json({
+      totalSongs,
+      totalArtists,
+      totalAlbums,
+      totalGenres,
+      songsByGenre,
+      songsByArtist,
+      songsByAlbum
+      // Add more statistics here as needed
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch statistics' });
+  }
+});
+
 
 module.exports = router;
